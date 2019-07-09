351cb30144461643bee1f55eab0d7f82edaa4621
guojian <guojian@weiapp.com>
2019-07-09 13:42:52 +0800

    firsh push

---
+++ config/redis.ts
@@ -0,0 +1,9 @@
+export let redisConfig = {
+  host: "redis.com",
+  port: 6379,
+  password: "5201314qv",
+  codeRegisterSuffix: "--register--Mobile",
+  codeRegisterTimeOut: "120",
+  codeLoginSuffix: "--login--Mobile",
+  codeLoginTimeOut: "120"
+};
